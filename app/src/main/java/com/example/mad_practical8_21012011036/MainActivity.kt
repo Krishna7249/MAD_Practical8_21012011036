67d5e0bfdec40d1068230f2d1758c62e8f510ea2
Krishna7249 <98690735+Krishna7249@users.noreply.github.com>
2023-09-19 09:19:48 +0530

    pr

--- app/src/main/java/com/example/mad_practical8_21012011036/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012011036/MainActivity.kt
@@ -5,11 +5,8 @@ import android.os.Bundle
 import android.widget.TextClock
 
 class MainActivity : AppCompatActivity() {
-    lateinit var clockTC: TextClock
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        clockTC = findViewById(R.id.idTCClock)
-        clockTC.format24Hour="hh:mm:ss a"
     }
 }
