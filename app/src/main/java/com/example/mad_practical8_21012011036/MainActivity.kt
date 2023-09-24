989e03b6196143fbf393ec4b50714290b1f95d52
Krishna7249 <98690735+Krishna7249@users.noreply.github.com>
2023-09-24 21:06:28 +0530

    pr8

--- app/src/main/java/com/example/mad_practical8_21012011036/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012011036/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.mad_practical8_21012011036
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextClock
-import android.widget.TimePicker
+import android.widget.Button
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 
@@ -13,14 +15,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val card = findViewById<MaterialCardView>(R.id.card1)
-        val createAlarm = findViewById<MaterialButton>(R.id.create)
-        createAlarm.setOnClickListener{
-            TimePickerDialog(this,{tp,hour,minute -> setAlarmTime(hour,minute)},11,0,false).show()
-        }
 
+        val addAlarm : Button = findViewById(R.id.create)
+
+        val card : MaterialCardView = findViewById(R.id.card1)
+
+        card.visibility = View.GONE
+
+        addAlarm.setOnClickListener {
+            TimePickerDialog(this, {tp, hour, minute -> setAlarmTime(hour, minute)}, 11,0,false).show()
+        }
     }
-    fun setAlarmTime(hour:Int,minute:Int){
-        //card.visibility= View.GONE
+
+    fun setAlarmTime(hour : Int, minute : Int){
+        //card.visibility = View.VISIBLE
+    }
+
+    fun setAlarm(millisitime : Long, action : String) {
+        val intentalarm = Intent(applicationContext, AlarmBroadcastReceiver::class.java)
+        val pendingintent = PendingIntent.getBroadcast(
+            applicationContext,
+            4356,
+            intentalarm,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        // if(str == "Start"){
+        //AlarmManager.RTC_WAKEUP,
+        //millisitime,
+        //pendingintent
+        //}
+        //else if(str == "Stop"){
+          //  alarmManager.cancel(pendingintent)
+        //}
     }
 }
